d3efa4ad5258ab5b8a8d628a89959e3e0ebe2f7a
Norbert Sztyk <norbert2000@onet.eu>
2020-11-02 21:14:40 +0100

    Reorganize project structure to fit my configuration

---
+++ 6.Props-Custom-Events/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>{{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Norbert",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif, Helvetica, Arial,
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin: 60px auto;
+}
+</style>
